3d2cd882f5bb98effbc5893513ac9718e29bef97
Noor Mulla <noor.mulla@teranet.ca>
2023-05-18 20:25:04 +0000

    initial commit k8s cluster terraform project

--- main.tf
+++ main.tf
@@ -16,11 +16,7 @@ resource "azurerm_kubernetes_cluster" "default" {
      client_secret = var.ARM_CLIENT_SECRET
  }
 
- role_based_access_control {
-     enabled = true
- }
-
- tags = {
+tags = {
      studentid = var.prefix
  }
  }
